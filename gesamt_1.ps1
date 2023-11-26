79c91ccba24926c7b960fa7e4148f9455fa9aa5c
pewox <117673001+pewox@users.noreply.github.com>
2023-11-26 19:08:48 +0100

    Update gesamt_1.ps1

--- gesamt_1.ps1
+++ gesamt_1.ps1
@@ -2,31 +2,36 @@ $write = '-536805376', 'modify', 'Write', 'Change'
 $full = '268435456', 'FullControl', 'Full'
 $read = '-1610612736', 'ReadAndExecute', 'Read'
 $exclude = 'VORDEFINIERT\Administratoren', 'VORDEFINIERT\Benutzer', 'NT-AUTORITÄT\SYSTEM', 'NT-AUTORITÄT\Authentifizierte Benutzer', 'NT SERVICE\TrustedInstaller'
+$jeder = 'Jeder', 'everyone'
 
-$share_obj = (Get-SmbShare -Special $false | get-smbshareaccess).Where{$_.AccessControlType -eq 'Allow' -and $_.AccountName -notin $exclude}.ForEach{
-        [PSCustomObject]@{
-            computer = $env:COMPUTERNAME
-            share = $_.Name
-            account = $_.AccountName
-            share_right = $_.AccessRight
-        }
+$i = 0
+$share_obj = (Get-SmbShare -Special $false | get-smbshareaccess | sort Name).Where({$_.AccessControlType -eq 'Allow' -and $_.AccountName -notin $exclude}).ForEach{
+    [PSCustomObject]@{
+        Computer = $env:COMPUTERNAME
+        Share = $_.Name
+        Account = $_.AccountName
+        Access_Right = $_.AccessRight
+        id = 's_'+$i
     }
+    $i++
+}
 
 $shares = Get-SmbShare -Special $false
-$ntfs_obj = foreach($share in $shares){($share | Get-Acl).Access.where{$_.IdentityReference -notin $exclude -and $_.IdentityReference -notlike 'S-*-*-*-*'}.foreach{
+$j = 0
+$ntfs_obj = foreach($share in $shares){($share | Get-Acl | sort Path).Access.where({$_.IdentityReference -notin $exclude -and $_.IdentityReference -notlike 'S-*-*-*-*'}).foreach{
     [PSCustomObject]@{
-        computer = $env:COMPUTERNAME
-        share = $share.Name
-        account = $_.IdentityReference
-        NTFS_right = $_.FileSystemRights
+        Computer = $env:COMPUTERNAME
+        Share = $share.Name
+        Account = $_.IdentityReference
+        NTFS_Right = $_.FileSystemRights
+        id = 'n_'+$j
         }
+        $j++
     }
-}
-#$ntfs_obj
-#$share_obj
-$ntfs_obj.Where{$_.share -eq 'test2'}.foreach{
-    $_.NTFS_right
-}
-$share_obj.Where{$_.share -eq 'test2'}.foreach{
-    $_.share_right
-}
+} 
+$full_share = $share_obj.Where({$_.account -in $jeder -and $_.Access_Right -in $full })
+$read_share = $share_obj.Where({$_.account -in $jeder -and $_.Access_Right -in $read })
+$write_share = $share_obj.Where({$_.account -in $jeder -and $_.Access_Right -in $write })
+$read_share | Format-Table
+#$write_share
+#$full_share
