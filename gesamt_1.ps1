60215a5741f4009343abbf1417b4e8630f7fdb83
pewox <117673001+pewox@users.noreply.github.com>
2023-11-27 16:23:29 +0100

    Update gesamt_1.ps1

--- gesamt_1.ps1
+++ gesamt_1.ps1
@@ -1,39 +1,39 @@
 $write = '-536805376', 'modify', 'Write', 'Change'
 $full = '268435456', 'FullControl', 'Full'
 $read = '-1610612736', 'ReadAndExecute', 'Read'
-$exclude = 'VORDEFINIERT\Administratoren', 'VORDEFINIERT\Benutzer', 'NT-AUTORITÄT\SYSTEM', 'NT-AUTORITÄT\Authentifizierte Benutzer', 'NT SERVICE\TrustedInstaller'
-$jeder = 'Jeder', 'everyone'
+$exclude = 'NT-AUTORITÄT\SYSTEM', 'VORDEFINIERT\Administratoren', 'NT-AUTORITÄT\Authentifizierte Benutzer', 'VORDEFINIERT\Benutzer', 'ERSTELLER-BESITZER',
+'NT AUTHORITY\SYSTEM', 'BUILTIN\Administrators', 'BUILTIN\Users', 'CREATOR OWNER'
+$jeder = 'Jeder', 'Everyone'
 
 $shares = Get-SmbShare -Special $false
-$i = 0
-$share_obj = ($shares | get-smbshareaccess | sort Name).Where({$_.AccessControlType -eq 'Allow' -and $_.AccountName -notin $exclude}).ForEach{
+$share_obj = ($shares | get-smbshareaccess).Where({$_.AccessControlType -eq 'Allow' -and $_.AccountName -notin $exclude}).ForEach{
     [PSCustomObject]@{
         Computer = $env:COMPUTERNAME
         Share = $_.Name
         Account = $_.AccountName
         Access_Right = $_.AccessRight
-        id = 's_'+$i
+        id = $_.Name + ':' + $_.AccountName
     }
-    $i++
 }
-
-$j = 0
-$ntfs_obj = foreach($share in $shares){($share | Get-Acl | sort Path).Access.where({$_.IdentityReference -notin $exclude -and $_.IdentityReference -notlike 'S-*-*-*-*'}).foreach{
+$ntfs_obj = foreach($share in $shares){($share | Get-Acl).Access.where({$_.IdentityReference -notin $exclude -and $_.IdentityReference -notlike 'S-*-*-*-*'}).foreach{
     [PSCustomObject]@{
         Computer = $env:COMPUTERNAME
         Share = $share.Name
         Account = $_.IdentityReference
         NTFS_Right = $_.FileSystemRights
-        id = 'n_'+$j
+        id = $share.Name + ':' + $_.IdentityReference
+        
         }
-        $j++
     }
 } 
 
-$full_share = $share_obj.Where({$_.account -in $jeder -and $_.Access_Right -in $full }).id
-$f_arr = foreach($val in $full_share){
-    $val.id
+$full_share_everyone = $share_obj.Where({$_.account -in $jeder -and $_.Access_Right -in $full })
+$write_share_everyone = $share_obj.Where({$_.account -in $jeder -and $_.Access_Right -in $write })
+$read_share_everyone = $share_obj.Where({$_.account -in $jeder -and $_.Access_Right -in $read })
+
+foreach($val in $ntfs_obj){
+    if($val.id -in $full_share_everyone.id){
+        $val.id + ' status=2'
+    } else {'NO'}
 }
-$full_share
-$read_share = $share_obj.Where({$_.account -in $jeder -and $_.Access_Right -in $read })
-$write_share = $share_obj.Where({$_.account -in $jeder -and $_.Access_Right -in $write })
+
