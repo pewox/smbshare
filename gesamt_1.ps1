7db28221c6981642cd066198470cba64823d67f2
pewox <117673001+pewox@users.noreply.github.com>
2023-11-27 21:58:32 +0100

    Update gesamt_1.ps1

--- gesamt_1.ps1
+++ gesamt_1.ps1
@@ -1,18 +1,19 @@
-$write = '-536805376', 'modify', 'Write', 'Change'
+$change = '-536805376', 'modify', 'Write', 'Change'
 $full = '268435456', 'FullControl', 'Full'
 $read = '-1610612736', 'ReadAndExecute', 'Read'
-$exclude = 'NT-AUTORITÄT\SYSTEM', 'VORDEFINIERT\Administratoren', 'NT-AUTORITÄT\Authentifizierte Benutzer', 'VORDEFINIERT\Benutzer', 'ERSTELLER-BESITZER',
-'NT AUTHORITY\SYSTEM', 'BUILTIN\Administrators', 'BUILTIN\Users', 'CREATOR OWNER'
-$jeder = 'Jeder', 'Everyone'
+$exclude = 'VORDEFINIERT\Administratoren', 'VORDEFINIERT\Benutzer', 'NT-AUTORITÄT\SYSTEM', 'NT-AUTORITÄT\Authentifizierte Benutzer', 'NT SERVICE\TrustedInstaller', 'Users'
+$jeder = 'Jeder', 'everyone'
 
-$shares = Get-SmbShare -Special $false
+New-Item -Path '.\Freigaben_auslesen\out.txt' -Force
+
+$shares = (Get-SmbShare -Special $false).where({$_.Name -notin $exclude})
 $share_obj = ($shares | get-smbshareaccess).Where({$_.AccessControlType -eq 'Allow' -and $_.AccountName -notin $exclude}).ForEach{
     [PSCustomObject]@{
         Computer = $env:COMPUTERNAME
         Share = $_.Name
         Account = $_.AccountName
         Access_Right = $_.AccessRight
-        id = $_.Name + ':' + $_.AccountName
+        ID = $_.Name + ':' + $_.AccountName
     }
 }
 $ntfs_obj = foreach($share in $shares){($share | Get-Acl).Access.where({$_.IdentityReference -notin $exclude -and $_.IdentityReference -notlike 'S-*-*-*-*'}).foreach{
@@ -20,20 +21,19 @@ $ntfs_obj = foreach($share in $shares){($share | Get-Acl).Access.where({$_.Ident
         Computer = $env:COMPUTERNAME
         Share = $share.Name
         Account = $_.IdentityReference
-        NTFS_Right = $_.FileSystemRights
-        id = $share.Name + ':' + $_.IdentityReference
-        
+        NTFS_Right = $_.filesystemrights
+        ID = $share.Name + ':' + $_.IdentityReference
         }
     }
 } 
 
-$full_share_everyone = $share_obj.Where({$_.account -in $jeder -and $_.Access_Right -in $full })
-$write_share_everyone = $share_obj.Where({$_.account -in $jeder -and $_.Access_Right -in $write })
-$read_share_everyone = $share_obj.Where({$_.account -in $jeder -and $_.Access_Right -in $read })
-
+$full_share = $share_obj.Where({$_.Access_Right -in $full })
+$write_share = $share_obj.Where({$_.Access_Right -in $change})
+$read_share = $share_obj.Where({$_.Access_Right -in $read })
+#$full_share | Format-Table
 foreach($val in $ntfs_obj){
-    if($val.id -in $full_share_everyone.id){
-        $val.id + ' status=2'
-    } else {'NO'}
+    if($val.id -in $full_share.id){
+        Add-Content -Path '.\Freigaben_auslesen\out.txt' -Value ('status=2 {0}' -f $val.id)
+    } #else {'NO'}
 }
-
+$ntfs_obj | Format-Table
