956e901413bd33773ab572087aaa79a6984f3730
pewox <117673001+pewox@users.noreply.github.com>
2023-11-28 16:24:37 +0100

    Update part_smb.ps1

--- part_smb.ps1
+++ part_smb.ps1
@@ -1,16 +1,20 @@
 $write = '-536805376', 'modify', 'Write', 'Change'
 $full = '268435456', 'FullControl', 'Full'
 $read = '-1610612736', 'ReadAndExecute', 'Read'
-$exclude = 'VORDEFINIERT\Administratoren', 'VORDEFINIERT\Benutzer', 'NT-AUTORITÄT\SYSTEM', 'NT-AUTORITÄT\Authentifizierte Benutzer', 'NT SERVICE\TrustedInstaller', 'Users'
+$exclude = 'NT-AUTORITÄT\SYSTEM', 'VORDEFINIERT\Administratoren', 'NT-AUTORITÄT\Authentifizierte Benutzer', 'VORDEFINIERT\Benutzer', 'ERSTELLER-BESITZER',
+'NT AUTHORITY\SYSTEM', 'BUILTIN\Administrators', 'BUILTIN\Users', 'CREATOR OWNER'
+
+$path = '.\Freigaben_auslesen\out.txt'
+New-Item -Path $path -Force | Out-Null
 
 $shares = (Get-SmbShare -Special $false).where({$_.Name -notin $exclude})
-$share_obj = ($shares | get-smbshareaccess).Where({$_.AccessControlType -eq 'Allow' -and $_.AccountName -notin $exclude}).ForEach{
+$share_obj = ($shares | get-smbshareaccess).Where({$_.AccessControlType -eq 'Allow' -and $_.AccountName -in $jeder -and $_.AccountName -notin $exclude}).ForEach{
     [PSCustomObject]@{
         Computer = $env:COMPUTERNAME
         Share = $_.Name
         Account = $_.AccountName
         Access_Right = $_.AccessRight
-        id = $_.Name + ':' + $_.AccountName
+        ID = $_.Name + ':' + $_.AccountName
     }
 }
 $share_obj | Format-Table
