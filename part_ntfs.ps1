aa53860e31be9c15a85d1b31155d70fd23fe1810
pewox <117673001+pewox@users.noreply.github.com>
2023-11-28 16:25:12 +0100

    Update part_ntfs.ps1

--- part_ntfs.ps1
+++ part_ntfs.ps1
@@ -1,17 +1,18 @@
 $write = '-536805376', 'modify', 'Write', 'Change'
 $full = '268435456', 'FullControl', 'Full'
 $read = '-1610612736', 'ReadAndExecute', 'Read'
-$exclude = 'VORDEFINIERT\Administratoren', 'VORDEFINIERT\Benutzer', 'NT-AUTORITÄT\SYSTEM', 'NT-AUTORITÄT\Authentifizierte Benutzer', 'NT SERVICE\TrustedInstaller', 'Users'
+$exclude = 'NT-AUTORITÄT\SYSTEM', 'VORDEFINIERT\Administratoren', 'NT-AUTORITÄT\Authentifizierte Benutzer', 'VORDEFINIERT\Benutzer', 'ERSTELLER-BESITZER',
+'NT AUTHORITY\SYSTEM', 'BUILTIN\Administrators', 'BUILTIN\Users', 'CREATOR OWNER'
 
-$shares = (Get-SmbShare -Special $false).where({$_.Name -notin $exclude})
-$ntfs_obj = foreach($share in $shares){($share | Get-Acl).Access.where({$_.IdentityReference -notin $exclude -and $_.IdentityReference -notlike 'S-*-*-*-*'}).foreach{
+$path = '.\Freigaben_auslesen\out.txt'
+New-Item -Path $path -Force | Out-Null
+$ntfs_obj = foreach($share in $shares){($share | Get-Acl).Access.where({$_.IdentityReference -notin $exclude -and $_.IdentityReference -in $jeder}).foreach{
     [PSCustomObject]@{
         Computer = $env:COMPUTERNAME
         Share = $share.Name
         Account = $_.IdentityReference
-        NTFS_Right = $_.FileSystemRights
-        id = $share.Name + ':' + $_.IdentityReference
-        
+        NTFS_Right = $_.filesystemrights
+        ID = $share.Name + ':' + $_.IdentityReference
         }
     }
 } 
