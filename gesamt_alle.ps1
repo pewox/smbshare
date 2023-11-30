8313c6384b1e0a26a975fee92a2dbbf56a041411
pewox <117673001+pewox@users.noreply.github.com>
2023-11-30 21:14:47 +0100

    Update gesamt_alle.ps1

--- gesamt_alle.ps1
+++ gesamt_alle.ps1
@@ -3,10 +3,7 @@ $full = '268435456', 'FullControl', 'Full'
 $read = '-1610612736', 'ReadAndExecute', 'Read'
 $exclude = 'VORDEFINIERT\Administratoren', 'VORDEFINIERT\Benutzer', 'NT-AUTORITÄT\SYSTEM', 'NT-AUTORITÄT\Authentifizierte Benutzer', 'NT SERVICE\TrustedInstaller', 'ERSTELLER-BESITZER',
 'NT AUTHORITY\SYSTEM', 'BUILTIN\Administrators', 'BUILTIN\Users', 'CREATOR OWNER'
-$jeder = 'Jeder', 'Everyone'
-$users = 'Users', 'Benutzer'
-$gast = 'Gast', 'Guest'
-$alle = 'Jeder', 'Everyone', 'Users', 'Benutzer', 'Gast', 'Guest'
+$alle = 'Jeder', 'Everyone', $($env:COMPUTERNAME + '\Users'), $($env:COMPUTERNAME + '\Benutzer'), $($env:COMPUTERNAME + '\gast'), $($env:COMPUTERNAME + '\guest') #, 'VORDEFINIERT\Benutzer'
 
 $path = '.\out.txt'
 New-Item -Path $path -Force | Out-Null
@@ -14,8 +11,8 @@ New-Item -Path $path -Force | Out-Null
 $shares = (Get-SmbShare -Special $false).where({$_.Name -notin $exclude})
 $share_obj = ($shares | get-smbshareaccess).Where({$_.AccessControlType -eq 'Allow' -and $_.AccountName -in $alle -and $_.AccountName -notin $exclude}).ForEach{
     $t1 = if($_.AccountName -eq 'Everyone'){'Jeder'}
-        elseif($_.AccountName -eq 'Users'){'Benutzer'}
-        elseif ($_.AccountName -eq 'Guest') {'Gast'}
+        elseif($_.AccountName -eq $($env:COMPUTERNAME + '\Users')){$($env:COMPUTERNAME + '\Benutzer')}
+        elseif ($_.AccountName -eq $($env:COMPUTERNAME + '\guest')) {$($env:COMPUTERNAME + '\gast')}
         else{$_.AccountName}
     [PSCustomObject]@{
         Server = $env:COMPUTERNAME
@@ -28,8 +25,8 @@ $share_obj = ($shares | get-smbshareaccess).Where({$_.AccessControlType -eq 'All
 
 $ntfs_obj = foreach($share in $shares){($share | Get-Acl).Access.where({$_.IdentityReference -notin $exclude -and $_.IdentityReference -in $alle}).foreach{
     $t2 = if($_.IdentityReference -eq 'Everyone'){'Jeder'}
-        elseif($_.IdentityReference -eq 'Users'){'Benutzer'}
-        elseif($_.IdentityReference -eq 'Guest'){'Gast'}
+        elseif($_.IdentityReference -eq $($env:COMPUTERNAME + '\Users')){$($env:COMPUTERNAME + '\Benutzer')}
+        elseif($_.IdentityReference -eq $($env:COMPUTERNAME + '\guest')) {$($env:COMPUTERNAME + '\gast')}
         else{$_.IdentityReference}
     [PSCustomObject]@{
         Server = $env:COMPUTERNAME
@@ -42,17 +39,21 @@ $ntfs_obj = foreach($share in $shares){($share | Get-Acl).Access.where({$_.Ident
 } 
 
 $full_share_j = $share_obj.Where({$_.Share_Right -in $full })
-$full_share_j
 $change_share_j = $share_obj.Where({$_.Share_Right -in $change})
 $read_share_j = $share_obj.Where({$_.Share_Right -in $read })
 
 foreach($val in $ntfs_obj){
-    
-    if($val.id -in $full_share_j.id -and ($val.NTFS_Right -like '*Full*')){
+    if($val.id -in $full_share_j.id -and ($val.NTFS_Right -like '*Full*' -or $val.NTFS_Right -like '*268435456*')){
         Add-Content -Path $path -Value ('2 "{1}:Share={2}" {0} {3}' -f $val.id, $env:COMPUTERNAME, $val.Share, $val.NTFS_Right)
     } 
-    elseif ($val.id -in $change_share_j.id -or $val.id -in $read_share_j.id -or $val.id -in $full_share_j.id -and $val.NTFS_Right -notlike '*Full*') 
+    elseif (($val.id -in $change_share_j.id -or $val.id -in $read_share_j.id -or $val.id -in $full_share_j.id) -and ($val.NTFS_Right -notlike '*Full*' -or $val.NTFS_Right -notlike '*268435456*')) 
     {Add-Content -Path $path -Value ('0 "{1}:Share={2}" {0} {3}' -f $val.id, $env:COMPUTERNAME, $val.Share, $val.NTFS_Right)}
 }
-$ntfs_obj | Format-Table
-$share_obj | Format-Table
+#$ntfs_obj | Format-Table
+#$share_obj | Format-Table
+
+$full_share_b = $share_obj.Where({$_.Share_Right -in $full })
+#$full_share_b
+$change_share_j = $share_obj.Where({$_.Share_Right -in $change})
+$read_share_j = $share_obj.Where({$_.Share_Right -in $read })
+
