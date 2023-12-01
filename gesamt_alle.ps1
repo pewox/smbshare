1e295713074ed6dd30c44adb61396f0fb7865e87
pewox <117673001+pewox@users.noreply.github.com>
2023-12-01 10:28:18 +0100

    Update gesamt_alle.ps1

--- gesamt_alle.ps1
+++ gesamt_alle.ps1
@@ -1,18 +1,19 @@
 $change = '-536805376', 'modify', 'Write', 'Change'
 $full = '268435456', 'FullControl', 'Full'
 $read = '-1610612736', 'ReadAndExecute', 'Read'
-$exclude = 'VORDEFINIERT\Administratoren', 'VORDEFINIERT\Benutzer', 'NT-AUTORITÄT\SYSTEM', 'NT-AUTORITÄT\Authentifizierte Benutzer', 'NT SERVICE\TrustedInstaller', 'ERSTELLER-BESITZER',
-'NT AUTHORITY\SYSTEM', 'BUILTIN\Administrators', 'BUILTIN\Users', 'CREATOR OWNER'
-$alle = 'Jeder', 'Everyone', $($env:COMPUTERNAME + '\Users'), $($env:COMPUTERNAME + '\Benutzer'), $($env:COMPUTERNAME + '\gast'), $($env:COMPUTERNAME + '\guest') #, 'VORDEFINIERT\Benutzer'
+$exclude = 'VORDEFINIERT\Administratoren', 'NT-AUTORITÄT\SYSTEM', 'NT-AUTORITÄT\Authentifizierte Benutzer', 'NT SERVICE\TrustedInstaller', 'ERSTELLER-BESITZER',
+'NT AUTHORITY\SYSTEM', 'BUILTIN\Administrators', 'CREATOR OWNER'
+$alle = 'Jeder', 'Everyone', $($env:USERDOMAIN + '\Users'), $($env:USERDOMAIN + '\Benutzer'), $($env:USERDOMAIN + '\gast'), $($env:USERDOMAIN + '\guest') , 'VORDEFINIERT\Benutzer', 'BUILTIN\Users'
 
-$path = '.\out.txt'
+$path = '.\out2.txt'
 New-Item -Path $path -Force | Out-Null
 
 $shares = (Get-SmbShare -Special $false).where({$_.Name -notin $exclude})
+# Objekt für Freigabeberechtigungen anlegen; ID aus Sharename und Benutzerkontoname für Vergleiche bilden
 $share_obj = ($shares | get-smbshareaccess).Where({$_.AccessControlType -eq 'Allow' -and $_.AccountName -in $alle -and $_.AccountName -notin $exclude}).ForEach{
     $t1 = if($_.AccountName -eq 'Everyone'){'Jeder'}
-        elseif($_.AccountName -eq $($env:COMPUTERNAME + '\Users')){$($env:COMPUTERNAME + '\Benutzer')}
-        elseif ($_.AccountName -eq $($env:COMPUTERNAME + '\guest')) {$($env:COMPUTERNAME + '\gast')}
+        elseif($_.AccountName -eq $($env:USERDOMAIN + '\Users')){$($env:USERDOMAIN + '\Benutzer')}
+        elseif ($_.AccountName -eq $($env:USERDOMAIN + '\guest')) {$($env:USERDOMAIN + '\gast')}
         else{$_.AccountName}
     [PSCustomObject]@{
         Server = $env:COMPUTERNAME
@@ -22,11 +23,11 @@ $share_obj = ($shares | get-smbshareaccess).Where({$_.AccessControlType -eq 'All
         ID = $_.Name + ':' + $t1
     }
 }
-
+# Objekt für NTFS-Berechtigungen anlegen; ID aus Sharename und Benutzerkontoname für Vergleiche bilden
 $ntfs_obj = foreach($share in $shares){($share | Get-Acl).Access.where({$_.IdentityReference -notin $exclude -and $_.IdentityReference -in $alle}).foreach{
     $t2 = if($_.IdentityReference -eq 'Everyone'){'Jeder'}
-        elseif($_.IdentityReference -eq $($env:COMPUTERNAME + '\Users')){$($env:COMPUTERNAME + '\Benutzer')}
-        elseif($_.IdentityReference -eq $($env:COMPUTERNAME + '\guest')) {$($env:COMPUTERNAME + '\gast')}
+        elseif($_.IdentityReference -eq $($env:USERDOMAIN + '\Users')){$($env:USERDOMAIN + '\Benutzer')}
+        elseif($_.IdentityReference -eq $($env:USERDOMAIN + '\guest')) {$($env:USERDOMAIN + '\gast')}
         else{$_.IdentityReference}
     [PSCustomObject]@{
         Server = $env:COMPUTERNAME
@@ -37,23 +38,35 @@ $ntfs_obj = foreach($share in $shares){($share | Get-Acl).Access.where({$_.Ident
         }
     }
 } 
-
+# Accounts nach Freigabeberechtigungen Full, Change, Read sortieren
 $full_share_j = $share_obj.Where({$_.Share_Right -in $full })
 $change_share_j = $share_obj.Where({$_.Share_Right -in $change})
 $read_share_j = $share_obj.Where({$_.Share_Right -in $read })
 
 foreach($val in $ntfs_obj){
-    if($val.id -in $full_share_j.id -and ($val.NTFS_Right -like '*Full*' -or $val.NTFS_Right -like '*268435456*')){
+    # prüfen Freigabe- und NTFS-Berechtigung ob effektiv Jeder:Full
+    if($val.id -in $full_share_j.id -and $val.Account -eq 'Jeder' -and ($val.NTFS_Right -like '*Full*' -or $val.NTFS_Right -like '*268435456*')){
         Add-Content -Path $path -Value ('2 "{1}:Share={2}" {0} {3}' -f $val.id, $env:COMPUTERNAME, $val.Share, $val.NTFS_Right)
-    } 
-    elseif (($val.id -in $change_share_j.id -or $val.id -in $read_share_j.id -or $val.id -in $full_share_j.id) -and ($val.NTFS_Right -notlike '*Full*' -or $val.NTFS_Right -notlike '*268435456*')) 
+    } # prüfen Freigabe- und NTFS-Berechtigung ob effektiv Jeder:Read, Change; also kein Full
+    elseif (($val.Account -eq 'Jeder' -and $val.id -in $change_share_j.id -or $val.id -in $read_share_j.id -or $val.id -in $full_share_j.id) -and ($val.NTFS_Right -notlike '*Full*' -or $val.NTFS_Right -notlike '*268435456*'))
     {Add-Content -Path $path -Value ('0 "{1}:Share={2}" {0} {3}' -f $val.id, $env:COMPUTERNAME, $val.Share, $val.NTFS_Right)}
-}
+    # prüfen Freigabe- und NTFS-Berechtigung ob effektiv Benutzer:Full
+    elseif ($val.Account -eq $($env:USERDOMAIN + '\Benutzer') -and $val.id -in $full_share_j.id -and ($val.NTFS_Right -like '*Full*' -or $val.NTFS_Right -like '*268435456*')) {
+        Add-Content -Path $path -Value ('2 "{1}:Share={2}" {0} {3}' -f $val.id, $env:COMPUTERNAME, $val.Share, $val.NTFS_Right)
+    } # prüfen Freigabe- und NTFS-Berechtigung ob effektiv Benutzer:Change, Read; also kein Full
+    elseif (($val.Account -eq $($env:USERDOMAIN + '\Benutzer') -and $val.id -in $change_share_j.id -or $val.id -in $read_share_j.id -or $val.id -in $full_share_j.id) -and ($val.NTFS_Right -notlike '*Full*' -or $val.NTFS_Right -notlike '*268435456*'))
+    {Add-Content -Path $path -Value ('0 "{1}:Share={2}" {0} {3}' -f $val.id, $env:COMPUTERNAME, $val.Share, $val.NTFS_Right)}
+
+    # prüfen Freigabe- und NTFS-Berechtigung ob effektiv Gast:Full
+    elseif ($val.Account -eq $($env:USERDOMAIN + '\Gast') -and $val.id -in $full_share_j.id -and ($val.NTFS_Right -like '*Full*' -or $val.NTFS_Right -like '*268435456*')) {
+        Add-Content -Path $path -Value ('2 "{1}:Share={2}" {0} {3}' -f $val.id, $env:COMPUTERNAME, $val.Share, $val.NTFS_Right)
+    } # prüfen Freigabe- und NTFS-Berechtigung ob effektiv Gast:Change, Read; also kein Full
+    elseif (($val.Account -eq $($env:USERDOMAIN + '\Gast') -and $val.id -in $change_share_j.id -or $val.id -in $read_share_j.id -or $val.id -in $full_share_j.id) -and ($val.NTFS_Right -notlike '*Full*' -or $val.NTFS_Right -notlike '*268435456*'))
+    {Add-Content -Path $path -Value ('0 "{1}:Share={2}" {0} {3}' -f $val.id, $env:COMPUTERNAME, $val.Share, $val.NTFS_Right)}
+}   
 #$ntfs_obj | Format-Table
 #$share_obj | Format-Table
 
-$full_share_b = $share_obj.Where({$_.Share_Right -in $full })
-#$full_share_b
-$change_share_j = $share_obj.Where({$_.Share_Right -in $change})
-$read_share_j = $share_obj.Where({$_.Share_Right -in $read })
-
+#$read_share_j | Format-Table
+$ntfs_obj | Format-Table
+$share_obj | Format-Table
