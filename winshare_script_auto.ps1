b64ecece391f0643535deac5c4e824838151e6e0
pewox <117673001+pewox@users.noreply.github.com>
2023-12-21 14:56:31 +0100

    Update winshare_script_auto.ps1

--- winshare_script_auto.ps1
+++ winshare_script_auto.ps1
@@ -2,4 +2,4 @@ $execute = Add-Content 'G:\powershell_skripte\scripte_automatisch\datei.txt' -Va
 $tspan = (New-TimeSpan -minutes 1)
 $action = New-ScheduledTaskAction -execute {$execute}
 $trigger = New-ScheduledTaskTrigger -Once -At 13:25 -RepetitionInterval $tspan -RepetitionDuration (New-TimeSpan -days 1)
-Register-ScheduledTask -Action $action -Trigger $trigger -taskname 'winshare' -force
+Register-ScheduledTask -Action $action -Trigger $trigger -taskname 'winshare' -runlevel highest -force
