dbf4db5fc150f523aa49e294977d73dd66b1e9d5
pewox <117673001+pewox@users.noreply.github.com>
2024-01-25 23:40:22 +0100

    Add files via upload

---
+++ testpath.ps1
@@ -0,0 +1,10 @@
+﻿
+$testpath = Test-Path -Path 'c:\test\test.txt'
+if($testpath){
+    $content = Get-Content -Path 'c:\test\test.txt'
+	if ($content){
+		Write-Host 'Inhalt vorhanden'
+	} else {
+		Write-Host 'kein Inhalt'
+	}
+}
